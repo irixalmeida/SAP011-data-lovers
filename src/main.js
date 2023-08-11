e7d9ac6c740a7ccbe505f686f3d0c3e3fb3d4258
irixalmeida <irinhaalmeida222@gmail.com>
2023-08-11 12:17:13 -0300

    Estrutura para printar um card na tela
    Co-authored-by: belatenorio isabelacandido.0481@aluno.saojudas.br

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import { example } from './data.js';
+import { example } from "./data.js";
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from "./data/ghibli/ghibli.js";
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+console.log(data.films[0].title);
+console.log(data.films[0].description);
+document.querySelector("#title").textContent = data.films[0].title;
+
+document.querySelector("#description").textContent = data.films[0].description;
+
+document.querySelector("#poster").setAttribute("src", data.films[0].poster);
