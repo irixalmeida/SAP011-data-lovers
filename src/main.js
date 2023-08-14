46f62f693451e7bcf31a8f216f886cef385e12d6
irixalmeida <irinhaalmeida222@gmail.com>
2023-08-13 23:18:29 -0300

    adiciona cards dinamicamente usando templates strings, adiciona estilização para cards

--- src/main.js
+++ src/main.js
@@ -3,12 +3,26 @@ import { example } from "./data.js";
 import data from "./data/ghibli/ghibli.js";
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+function createFilmItem(film) {
+  const filmItem = `
+    <div class="film-item">
+        <div class="film-item-front">
+            <img src="${film.poster}" class="film-poster">
+            <h3>${film.title}</h3>
+        </div>
+        <div class="film-item-back hide">
+            <p>${film.release_date}</p>
+        </div>
+    </div>
+  `;
 
-console.log(data.films[0].title);
-console.log(data.films[0].description);
-document.querySelector("#title").textContent = data.films[0].title;
+  return filmItem;
+}
 
-document.querySelector("#description").textContent = data.films[0].description;
+const listFilms = data.films.map((film) => {
+  const templateItem = createFilmItem(film);
 
-document.querySelector("#poster").setAttribute("src", data.films[0].poster);
+  return templateItem;
+});
+
+document.querySelector("#list").innerHTML = listFilms.join("");
